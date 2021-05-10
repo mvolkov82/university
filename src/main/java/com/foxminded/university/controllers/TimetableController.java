ff69a3265f9ae83b68507b1243938c119744fba4
Mikhail Volkov <leopard@inbox.ru>
2021-05-10 20:11:13 +0300

    Comment deleted.

--- src/main/java/com/foxminded/university/controllers/TimetableController.java
+++ src/main/java/com/foxminded/university/controllers/TimetableController.java
@@ -71,8 +71,6 @@ public class TimetableController {
     @GetMapping
     public String getAll(Model model) {
         List<TimetableItemDTO> dto = timetableItemService.getAllTimetableDTOS();
-//        String date = LocalDate.now().toString();
-//        List<TimetableItemDTO> dto = timetableItemService.getAllNotDeletedByDateAndGroupAndTeacherDTOS(date, date, null, null);
         model.addAttribute("timetables", dto);
         TimetableSearchingFilterDTO filterDTO = new TimetableSearchingFilterDTO();
         model.addAttribute("filter", filterDTO);
